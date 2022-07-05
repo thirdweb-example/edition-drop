b15afad1abc23e2e14fda3a3d9d8407b47619e94
Jarrod Watts <jarrodwatts16@gmail.com>
2022-07-05 11:59:07 +1000

    Edition Drop changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,25 @@
 import {
   ChainId,
-  useClaimedNFTSupply,
   useContractMetadata,
   useNetwork,
-  useNFTDrop,
-  useUnclaimedNFTSupply,
   useActiveClaimCondition,
+  useEditionDrop,
+  useNFT,
+  ThirdwebNftMedia,
 } from "@thirdweb-dev/react";
 import { useNetworkMismatch } from "@thirdweb-dev/react";
 import { useAddress, useMetamask } from "@thirdweb-dev/react";
+import { BigNumber } from "ethers";
 import type { NextPage } from "next";
 import { useState } from "react";
 import styles from "../styles/Theme.module.css";
 
-// Put Your NFT Drop Contract address from the dashboard here
-const myNftDropContractAddress = "0x322067594DBCE69A9a9711BC393440aA5e3Aaca1";
+// Put Your Edition Drop Contract address from the dashboard here
+const myEditionDropContractAddress =
+  "0x11232C2cd1757C3e4f78dcda318Bdfc6Bc5873A3";
 
 const Home: NextPage = () => {
-  const nftDrop = useNFTDrop(myNftDropContractAddress);
+  const editionDrop = useEditionDrop(myEditionDropContractAddress);
   const address = useAddress();
   const connectWithMetamask = useMetamask();
   const isOnWrongNetwork = useNetworkMismatch();
@@ -29,19 +31,24 @@ const Home: NextPage = () => {
 
   // Load contract metadata
   const { data: contractMetadata } = useContractMetadata(
-    myNftDropContractAddress
+    myEditionDropContractAddress
   );
 
-  // Load claimed supply and unclaimed supply
-  const { data: unclaimedSupply } = useUnclaimedNFTSupply(nftDrop);
-  const { data: claimedSupply } = useClaimedNFTSupply(nftDrop);
-  const isSoldOut = unclaimedSupply?.toNumber() === 0;
+  const { data: nftMetadata } = useNFT(editionDrop, 0);
 
   // Load the active claim condition
-  const { data: activeClaimCondition } = useActiveClaimCondition(nftDrop);
+  const { data: activeClaimCondition } = useActiveClaimCondition(
+    editionDrop,
+    BigNumber.from(0)
+  );
+
+  console.log({
+    contractMetadata,
+    activeClaimCondition,
+  });
 
   // Loading state while we fetch the metadata
-  if (!nftDrop || !contractMetadata) {
+  if (!editionDrop || !contractMetadata) {
     return <div className={styles.container}>Loading...</div>;
   }
 
@@ -62,7 +69,7 @@ const Home: NextPage = () => {
     setClaiming(true);
 
     try {
-      const minted = await nftDrop?.claim(quantity);
+      const minted = await editionDrop?.claim(0, quantity);
       console.log(minted);
       alert(`Successfully minted NFT${quantity > 1 ? "s" : ""}!`);
     } catch (error: any) {
@@ -85,10 +92,10 @@ const Home: NextPage = () => {
 
         <div className={styles.imageSide}>
           {/* Image Preview of NFTs */}
-          <img
+          <ThirdwebNftMedia
+            // @ts-ignore
+            metadata={nftMetadata?.metadata}
             className={styles.image}
-            src={contractMetadata?.image}
-            alt={`${contractMetadata?.name} preview image`}
           />
 
           {/* Amount claimed so far */}
@@ -97,15 +104,12 @@ const Home: NextPage = () => {
               <p>Total Minted</p>
             </div>
             <div className={styles.mintAreaRight}>
-              {claimedSupply && unclaimedSupply ? (
+              {activeClaimCondition ? (
                 <p>
                   {/* Claimed supply so far */}
-                  <b>{claimedSupply?.toNumber()}</b>
+                  <b>{activeClaimCondition.currentMintSupply}</b>
                   {" / "}
-                  {
-                    // Add unclaimed and claimed supply to get the total supply
-                    claimedSupply?.toNumber() + unclaimedSupply?.toNumber()
-                  }
+                  {activeClaimCondition.maxQuantity}
                 </p>
               ) : (
                 // Show loading state if we're still loading the supply
@@ -116,48 +120,41 @@ const Home: NextPage = () => {
 
           {/* Show claim button or connect wallet button */}
           {address ? (
-            // Sold out or show the claim button
-            isSoldOut ? (
-              <div>
-                <h2>Sold Out</h2>
-              </div>
-            ) : (
-              <>
-                <p>Quantity</p>
-                <div className={styles.quantityContainer}>
-                  <button
-                    className={`${styles.quantityControlButton}`}
-                    onClick={() => setQuantity(quantity - 1)}
-                    disabled={quantity <= 1}
-                  >
-                    -
-                  </button>
+            <>
+              <p>Quantity</p>
+              <div className={styles.quantityContainer}>
+                <button
+                  className={`${styles.quantityControlButton}`}
+                  onClick={() => setQuantity(quantity - 1)}
+                  disabled={quantity <= 1}
+                >
+                  -
+                </button>
 
-                  <h4>{quantity}</h4>
-
-                  <button
-                    className={`${styles.quantityControlButton}`}
-                    onClick={() => setQuantity(quantity + 1)}
-                    disabled={
-                      quantity >=
-                      parseInt(
-                        activeClaimCondition?.quantityLimitPerTransaction || "0"
-                      )
-                    }
-                  >
-                    +
-                  </button>
-                </div>
+                <h4>{quantity}</h4>
 
                 <button
-                  className={`${styles.mainButton} ${styles.spacerTop} ${styles.spacerBottom}`}
-                  onClick={mint}
-                  disabled={claiming}
+                  className={`${styles.quantityControlButton}`}
+                  onClick={() => setQuantity(quantity + 1)}
+                  disabled={
+                    quantity >=
+                    parseInt(
+                      activeClaimCondition?.quantityLimitPerTransaction || "0"
+                    )
+                  }
                 >
-                  {claiming ? "Minting..." : "Mint"}
+                  +
                 </button>
-              </>
-            )
+              </div>
+
+              <button
+                className={`${styles.mainButton} ${styles.spacerTop} ${styles.spacerBottom}`}
+                onClick={mint}
+                disabled={claiming}
+              >
+                {claiming ? "Minting..." : "Mint"}
+              </button>
+            </>
           ) : (
             <button className={styles.mainButton} onClick={connectWithMetamask}>
               Connect Wallet
