98803b15213915242bb3e75363495defd7c96ab1
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2022-11-22 06:31:33 +0530

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,135 +1,326 @@
 import {
-  useActiveClaimCondition,
+  useActiveClaimConditionForWallet,
+  useAddress,
+  useClaimConditions,
+  useClaimerProofs,
+  useClaimIneligibilityReasons,
   useContract,
   useContractMetadata,
-  useNFT,
+  useTotalCirculatingSupply,
   Web3Button,
 } from "@thirdweb-dev/react";
+import { BigNumber, utils } from "ethers";
 import type { NextPage } from "next";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import styles from "../styles/Theme.module.css";
+import { parseIneligibility } from "../utils/parseIneligibility";
 
 // Put Your Edition Drop Contract address from the dashboard here
 const myEditionDropContractAddress =
-  "0x4894833987b4E9629d1Bc7ebdAB21a56F5c09a36";
+  "0xDC8017E1E20BFF80a49B0B92F719f00170013B4F";
 
 // Put your token ID here
 const tokenId = 0;
 
 const Home: NextPage = () => {
+  const address = useAddress();
+  const [quantity, setQuantity] = useState(1);
   const { contract: editionDrop } = useContract(myEditionDropContractAddress);
-
-  // The amount the user claims, updates when they type a value into the input field.
-  const [quantity, setQuantity] = useState<number>(1); // default to 1
-
-  // Load contract metadata
   const { data: contractMetadata } = useContractMetadata(editionDrop);
 
-  // Load the NFT metadata
-  const { data: nftMetadata } = useNFT(editionDrop, tokenId);
-
-  // Load the active claim condition
-  const { data: activeClaimCondition } = useActiveClaimCondition(
+  const claimConditions = useClaimConditions(editionDrop);
+  const activeClaimCondition = useActiveClaimConditionForWallet(
     editionDrop,
+    address,
+    tokenId
+  );
+  const claimerProofs = useClaimerProofs(editionDrop, address || "", tokenId);
+  const claimIneligibilityReasons = useClaimIneligibilityReasons(
+    editionDrop,
+    {
+      quantity,
+      walletAddress: address || "",
+    },
     tokenId
   );
 
-  // Loading state while we fetch the metadata
-  if (!editionDrop || !contractMetadata) {
-    return <div className={styles.container}>Loading...</div>;
-  }
+  const claimedSupply = useTotalCirculatingSupply(editionDrop, tokenId);
+
+  const totalAvailableSupply = useMemo(() => {
+    try {
+      return BigNumber.from(activeClaimCondition.data?.availableSupply || 0);
+    } catch {
+      return BigNumber.from(1_000_000);
+    }
+  }, [activeClaimCondition.data?.availableSupply]);
+
+  const numberClaimed = useMemo(() => {
+    return BigNumber.from(claimedSupply.data || 0).toString();
+  }, [claimedSupply]);
+
+  const numberTotal = useMemo(() => {
+    const n = totalAvailableSupply.add(BigNumber.from(claimedSupply.data || 0));
+    if (n.gte(1_000_000)) {
+      return "";
+    }
+    return n.toString();
+  }, [totalAvailableSupply, claimedSupply]);
+
+  const priceToMint = useMemo(() => {
+    const bnPrice = BigNumber.from(
+      activeClaimCondition.data?.currencyMetadata.value || 0
+    );
+    return `${utils.formatUnits(
+      bnPrice.mul(quantity).toString(),
+      activeClaimCondition.data?.currencyMetadata.decimals || 18
+    )} ${activeClaimCondition.data?.currencyMetadata.symbol}`;
+  }, [
+    activeClaimCondition.data?.currencyMetadata.decimals,
+    activeClaimCondition.data?.currencyMetadata.symbol,
+    activeClaimCondition.data?.currencyMetadata.value,
+    quantity,
+  ]);
+
+  const maxClaimable = useMemo(() => {
+    let bnMaxClaimable;
+    try {
+      bnMaxClaimable = BigNumber.from(
+        activeClaimCondition.data?.maxClaimableSupply || 0
+      );
+    } catch (e) {
+      bnMaxClaimable = BigNumber.from(1_000_000);
+    }
+
+    let perTransactionClaimable;
+    try {
+      perTransactionClaimable = BigNumber.from(
+        activeClaimCondition.data?.maxClaimablePerWallet || 0
+      );
+    } catch (e) {
+      perTransactionClaimable = BigNumber.from(1_000_000);
+    }
+
+    if (perTransactionClaimable.lte(bnMaxClaimable)) {
+      bnMaxClaimable = perTransactionClaimable;
+    }
+
+    const snapshotClaimable = claimerProofs.data?.maxClaimable;
+
+    if (snapshotClaimable) {
+      if (snapshotClaimable === "0") {
+        // allowed unlimited for the snapshot
+        bnMaxClaimable = BigNumber.from(1_000_000);
+      } else {
+        try {
+          bnMaxClaimable = BigNumber.from(snapshotClaimable);
+        } catch (e) {
+          // fall back to default case
+        }
+      }
+    }
+
+    let max;
+    if (totalAvailableSupply.lt(bnMaxClaimable)) {
+      max = totalAvailableSupply;
+    } else {
+      max = bnMaxClaimable;
+    }
+
+    if (max.gte(1_000_000)) {
+      return 1_000_000;
+    }
+    return max.toNumber();
+  }, [
+    claimerProofs.data?.maxClaimable,
+    totalAvailableSupply,
+    activeClaimCondition.data?.maxClaimableSupply,
+    activeClaimCondition.data?.maxClaimablePerWallet,
+  ]);
+
+  const isSoldOut = useMemo(() => {
+    try {
+      return (
+        (activeClaimCondition.isSuccess &&
+          BigNumber.from(activeClaimCondition.data?.availableSupply || 0).lte(
+            0
+          )) ||
+        numberClaimed === numberTotal
+      );
+    } catch (e) {
+      return false;
+    }
+  }, [
+    activeClaimCondition.data?.availableSupply,
+    activeClaimCondition.isSuccess,
+    numberClaimed,
+    numberTotal,
+  ]);
+
+  const canClaim = useMemo(() => {
+    return (
+      activeClaimCondition.isSuccess &&
+      claimIneligibilityReasons.isSuccess &&
+      claimIneligibilityReasons.data?.length === 0 &&
+      !isSoldOut
+    );
+  }, [
+    activeClaimCondition.isSuccess,
+    claimIneligibilityReasons.data?.length,
+    claimIneligibilityReasons.isSuccess,
+    isSoldOut,
+  ]);
+
+  const isLoading = useMemo(() => {
+    return (
+      activeClaimCondition.isLoading || claimedSupply.isLoading || !editionDrop
+    );
+  }, [activeClaimCondition.isLoading, editionDrop, claimedSupply.isLoading]);
+
+  const buttonLoading = useMemo(
+    () => isLoading || claimIneligibilityReasons.isLoading,
+    [claimIneligibilityReasons.isLoading, isLoading]
+  );
+  const buttonText = useMemo(() => {
+    if (isSoldOut) {
+      return "Sold Out";
+    }
+
+    if (canClaim) {
+      const pricePerToken = BigNumber.from(
+        activeClaimCondition.data?.currencyMetadata.value || 0
+      );
+      if (pricePerToken.eq(0)) {
+        return "Mint (Free)";
+      }
+      return `Mint (${priceToMint})`;
+    }
+    if (claimIneligibilityReasons.data?.length) {
+      return parseIneligibility(claimIneligibilityReasons.data, quantity);
+    }
+    if (buttonLoading) {
+      return "Checking eligibility...";
+    }
+
+    return "Claiming not available";
+  }, [
+    isSoldOut,
+    canClaim,
+    claimIneligibilityReasons.data,
+    buttonLoading,
+    activeClaimCondition.data?.currencyMetadata.value,
+    priceToMint,
+    quantity,
+  ]);
 
   return (
     <div className={styles.container}>
       <div className={styles.mintInfoContainer}>
-        <div className={styles.infoSide}>
-          {/* Title of your NFT Collection */}
-          <h1>{contractMetadata?.name}</h1>
-          {/* Description of your NFT Collection */}
-          <p className={styles.description}>{contractMetadata?.description}</p>
-        </div>
-
-        <div className={styles.imageSide}>
-          {/* Image Preview of NFTs */}
-          <img
-            className={styles.image}
-            src={nftMetadata?.metadata?.image || ""}
-            alt={`${nftMetadata?.metadata?.name} preview image`}
-          />
-
-          {/* Amount claimed so far */}
-          <div className={styles.mintCompletionArea}>
-            <div className={styles.mintAreaLeft}>
-              <p>Total Minted</p>
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <>
+            <div className={styles.infoSide}>
+              {/* Title of your NFT Collection */}
+              <h1>{contractMetadata?.name}</h1>
+              {/* Description of your NFT Collection */}
+              <p className={styles.description}>
+                {contractMetadata?.description}
+              </p>
             </div>
-            <div className={styles.mintAreaRight}>
-              {activeClaimCondition ? (
-                <p>
-                  {/* Claimed supply so far */}
-                  <b>{activeClaimCondition.currentMintSupply}</b>
-                  {" / "}
-                  {activeClaimCondition.availableSupply === "unlimited"
-                    ? "∞"
-                    : activeClaimCondition.maxClaimableSupply}
-                </p>
+
+            <div className={styles.imageSide}>
+              {/* Image Preview of NFTs */}
+              <img
+                className={styles.image}
+                src={contractMetadata?.image}
+                alt={`${contractMetadata?.name} preview image`}
+              />
+
+              {/* Amount claimed so far */}
+              <div className={styles.mintCompletionArea}>
+                <div className={styles.mintAreaLeft}>
+                  <p>Total Minted</p>
+                </div>
+                <div className={styles.mintAreaRight}>
+                  {claimedSupply ? (
+                    <p>
+                      <b>{numberClaimed}</b>
+                      {" / "}
+                      {numberTotal || "∞"}
+                    </p>
+                  ) : (
+                    // Show loading state if we're still loading the supply
+                    <p>Loading...</p>
+                  )}
+                </div>
+              </div>
+
+              {claimConditions.data?.length === 0 ||
+              claimConditions.data?.every(
+                (cc) => cc.maxClaimableSupply === "0"
+              ) ? (
+                <div>
+                  <h2>
+                    This drop is not ready to be minted yet. (No claim condition
+                    set)
+                  </h2>
+                </div>
               ) : (
-                // Show loading state if we're still loading the supply
-                <p>Loading...</p>
+                <>
+                  <p>Quantity</p>
+                  <div className={styles.quantityContainer}>
+                    <button
+                      className={`${styles.quantityControlButton}`}
+                      onClick={() => setQuantity(quantity - 1)}
+                      disabled={quantity <= 1}
+                    >
+                      -
+                    </button>
+
+                    <h4>{quantity}</h4>
+
+                    <button
+                      className={`${styles.quantityControlButton}`}
+                      onClick={() => setQuantity(quantity + 1)}
+                      disabled={quantity >= maxClaimable}
+                    >
+                      +
+                    </button>
+                  </div>
+
+                  <div className={styles.mintContainer}>
+                    {isSoldOut ? (
+                      <div>
+                        <h2>Sold Out</h2>
+                      </div>
+                    ) : (
+                      <Web3Button
+                        contractAddress={editionDrop?.getAddress() || ""}
+                        action={(cntr) => cntr.erc721.claim(quantity)}
+                        isDisabled={!canClaim || buttonLoading}
+                        onError={(err) => {
+                          console.error(err);
+                          alert("Error claiming NFTs");
+                        }}
+                        onSuccess={() => {
+                          setQuantity(1);
+                          alert("Successfully claimed NFTs");
+                        }}
+                      >
+                        {buttonLoading ? "Loading..." : buttonText}
+                      </Web3Button>
+                    )}
+                  </div>
+                </>
               )}
             </div>
-          </div>
-
-          {/* Show claim button or connect wallet button */}
-          <>
-            <p>Quantity</p>
-            <div className={styles.quantityContainer}>
-              <button
-                className={`${styles.quantityControlButton}`}
-                onClick={() => setQuantity(quantity - 1)}
-                disabled={quantity <= 1}
-              >
-                -
-              </button>
-
-              <h4>{quantity}</h4>
-
-              <button
-                className={`${styles.quantityControlButton}`}
-                onClick={() => setQuantity(quantity + 1)}
-                disabled={
-                  quantity >=
-                  parseInt(
-                    activeClaimCondition?.maxClaimablePerWallet.toString() ||
-                      "0"
-                  )
-                }
-              >
-                +
-              </button>
-            </div>
-            <div className={styles.mintContainer}>
-              <Web3Button
-                contractAddress={myEditionDropContractAddress}
-                action={async (contract) =>
-                  await contract.erc1155.claim(tokenId, quantity)
-                }
-                // If the function is successful, we can do something here.
-                onSuccess={() => alert("Claimed!")}
-                // If the function fails, we can do something here.
-                onError={(error) => alert(error?.message)}
-                accentColor="#f213a4"
-                colorMode="dark"
-              >
-                Mint {quantity} NFT{quantity > 1 ? "s" : ""}
-              </Web3Button>
-            </div>
           </>
-        </div>
+        )}
       </div>
       {/* Powered by thirdweb */}{" "}
       <img
         src="/logo.png"
-        alt="Thirdweb Logo"
+        alt="thirdweb Logo"
         width={135}
         className={styles.buttonGapTop}
       />
