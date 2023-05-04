1902518d9cf8b6c01c4eee3fa009ea67a9d9997a
Bankk <106103625+BankkRoll@users.noreply.github.com>
2023-05-04 18:39:03 -0400

    Remove unused import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from "next/app";
-import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 import Head from "next/head";
 import ThirdwebGuideFooter from "../components/GitHubLink";
